6fdbe33d55b61088e4595164ea551e01636a8b18
Vadim <varem99@mail.ru>
2020-04-01 19:42:33 +0300

    exchange

--- Lock/TTAS/spin_ttas.cpp
+++ Lock/TTAS/spin_ttas.cpp
@@ -6,10 +6,10 @@ void Spin_lock_tas::lock(){
 	{
 		/* First test the lock without invalidating
             any cache lines */
-		while(spin_.load());
+		while(spin_.load())
 			std::this_thread::yield();
 		expected = 0;
-	}while(!spin_.compare_exchange_weak(expected, 1));
+	}while(spin_.exchange(1, std::memory_order_acquire));
 }
 
 void Spin_lock_tas::unlock(){
@@ -17,6 +17,10 @@ void Spin_lock_tas::unlock(){
 	spin_.store(desired, std::memory_order_seq_cst);
 }
 
+// exchange
+//  139,97s user 0,36s system 358% cpu 39,115 total
+//  49,47s user 100,30s system 362% cpu 41,327 total -- yield
 
+//  compare_exchange_weak()
 //  144,57s user 0,14s system 365% cpu 39,629 total
 //  48,76s user 95,64s system 345% cpu 41,801 total -- yield
