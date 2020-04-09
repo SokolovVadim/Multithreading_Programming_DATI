dd999622257679d740cda7d0e6553751e35f2832
Vadim <varem99@mail.ru>
2020-04-09 09:25:19 +0300

    rw lock read-pref

--- RWLock/rw_lock.cpp
+++ RWLock/rw_lock.cpp
@@ -22,15 +22,33 @@ int main()
 
 	std::thread reader1(reader, 1);
 	std::thread reader2(reader, 2);
+	std::thread reader3(reader, 3);	
+	std::thread reader4(reader, 4);
+	std::thread reader5(reader, 5);
+	std::thread reader6(reader, 6);
+
 	std::thread writer1(writer, 1);
 	std::thread writer2(writer, 2);
+	std::thread writer3(writer, 3);
+	std::thread writer4(writer, 4);
+	std::thread writer5(writer, 5);
+	std::thread writer6(writer, 6);
 
 
 
 	reader1.join();
 	reader2.join();
+	reader3.join();
+	reader4.join();
+	reader5.join();
+	reader6.join();
+
 	writer1.join();
 	writer2.join();
+	writer3.join();
+	writer4.join();
+	writer5.join();
+	writer6.join();
 
 	sem_destroy(&rentry);
 	sem_destroy(&resource);
