cdff059fcadf441f78ba0334b13510f47ff0bca7
Vadim <varem99@mail.ru>
2020-04-12 21:22:55 +0300

    push_back pop_back

--- Template_List/List.hpp
+++ Template_List/List.hpp
@@ -5,7 +5,7 @@ template<typename T>
 struct Node
 {
  	T data;
- 	Node* next;
+ 	Node<T>* next;
 }; 
 
 //==========================================================
@@ -18,21 +18,115 @@ public:
 	List();
 	~List();
 
+	void push_back(const T& value);
+	void pop_back();
+	void display();
+	void loud_display();
+
 private:
 	Node<T>* head;
+	Node<T>* tail;
 };
+
+//==========================================================
 
 template<typename T>
 List<T>::List():
-	head(nullptr)
+	head(nullptr),
+	tail(nullptr)
 {
 	std::cout << "List constructed\n";
 }
+
+//==========================================================
 
 template<typename T>
 List<T>::~List()
 {
 	std::cout << "List destructed\n";
 }
+
+//==========================================================
+
+template<typename T>
+void List<T>::push_back(const T& value)
+{
+	Node<T>* temp = new Node<T>;
+	temp->data = value;
+	temp->next = nullptr;
+
+	if(head == nullptr)
+	{
+		head = temp;
+		tail = temp;
+	}
+	else
+	{
+		tail->next = temp;
+		tail = tail->next;
+	}
+}
+
+//==========================================================
+
+template<typename T>
+void List<T>::display()
+{
+	Node<T>* temp = head;
+
+	while(temp != nullptr)
+	{
+		std::cout << temp->data << "\t";
+		temp = temp->next;
+	}
+	std::cout << std::endl;
+}
+
+//==========================================================
+
+template<typename T>
+void List<T>::loud_display()
+{
+	Node<T>* temp = head;
+
+	while(temp != nullptr)
+	{
+		std::cout << temp->data << "\t" << temp << "\t" << temp->next << "\t" << head << "\t" << tail;
+		std::cout << std::endl;
+		temp = temp->next;
+	}
+}
+
+//==========================================================
+
+template<typename T>
+void List<T>::pop_back()
+{
+	// empty list
+	if (head == nullptr)
+    {
+        throw std::out_of_range("Can't pop from empty list");
+    }
+
+    // list with the only one element
+    if(head == tail)
+    {
+    	delete head;
+    	head = nullptr;
+    	tail = nullptr;
+    	return;
+    }
+
+	Node<T>* temp = head;
+	while(temp->next != tail)
+	{
+		temp = temp->next;
+	}
+	delete tail;
+	tail = temp;
+	tail->next = nullptr;
+}
+
+
 
 
--- Template_List/test.cpp
+++ Template_List/test.cpp
@@ -7,5 +7,42 @@ int main()
 	std::cout << "Hello!\n";
 	List<std::string> list;
 
+/*
+
+	list.push_back("It's");
+	list.push_back("corona");
+	list.push_back("time!");
+	list.display();
+	list.push_back("It's");
+	list.push_back("corona");
+	list.push_back("time");
+	list.push_back("right");
+	list.push_back("now");
+	list.display();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.display();
+	list.push_back("It's");
+	list.push_back("corona");
+	list.push_back("time");
+	list.push_back("right");
+	list.push_back("now");
+	list.display();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.pop_back();
+	list.display();
+	list.pop_back();
+	list.pop_back();
+	list.display();
+	list.pop_back();
+
+*/
+
 	return 0;
 }
