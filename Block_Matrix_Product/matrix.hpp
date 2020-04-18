a874248d2f6234884c8660645eeee141a62ed26f
Vadim <varem99@mail.ru>
2020-04-18 21:54:42 +0300

    read args

---
+++ Block_Matrix_Product/matrix.hpp
@@ -0,0 +1,13 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <climits>
+#include <errno.h>
+#include <cstdlib>
+#include <cstdio>
+long int ReadArg(char * str);
+
+
+
+
+#endif // MATRIX_HPP
---
+++ Block_Matrix_Product/reader.cpp
@@ -0,0 +1,30 @@
+#include "matrix.hpp"
+// read number from str
+
+long int ReadArg(char * str)
+{
+	char* endptr;
+	errno = 0;
+
+	long int number = strtol(str, &endptr, 10);
+
+	
+	if ((errno == ERANGE && (number == LONG_MAX || number == LONG_MIN)) || (errno != 0 && number == 0)) 
+	{
+       		perror("strtol");
+        	exit(EXIT_FAILURE);
+   	}
+
+	if (endptr == str)
+	{
+        	fprintf(stderr, "Error!\n");
+        	exit(EXIT_FAILURE);
+   	}
+	if (*endptr != '\0')
+	{
+        	fprintf(stderr, "Error!\n");
+        	exit(EXIT_FAILURE);
+   	}
+
+	return number;
+}
